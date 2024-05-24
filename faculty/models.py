5c9af179b94e074ff422c76d01cc71293a21b6ab
Frank Alimimian <alimimianfrank@gmail.com>
2024-05-24 14:09:39 +0100

    Minor update on the models

--- faculty/models.py
+++ faculty/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-User=get_user_model()
+User = get_user_model()
 # Create your models here.
 class Faculty(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    employee_id =models.CharField(max_length=10, unique=True)
-    qualification = models.CharField(max_length=255)
-    department = models.ForeignKey('department.Department', on_delete=models.SET_NULL, null=True)
-    courses = models.ManyToManyField('courses.Course', related_name='faculty')
+    name = models.CharField(max_length=255, unique=True ,default="Faculty of science") 
+    description = models.TextField(null=True, blank=True)
+    head = models.OneToOneField('FacultyHead', on_delete=models.SET_NULL, null=True, related_name="head_of_faculty")
     
     def __str__(self):
-        return f"{self.user.first_name} {self.user.last_name}"
+        return self.name
+    
+class FacultyHead(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    faculty = models.OneToOneField(Faculty, on_delete=models.CASCADE, related_name='head_of_faculty', null=True)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.user.first_name}  {self.user.last_name}"
+        
     
